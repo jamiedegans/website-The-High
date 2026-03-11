583790df9f034bc8272dda09297aaf370c929823
jamiedegans <1216596@student.roc-nijmegen.nl>
2026-03-11 11:43:38 +0100

    pushed the data to my database the menu
    need to add web compents for the menu to be called back in the website

--- menu.php
+++ menu.php
@@ -1,3 +1,42 @@
+<?php
+$host = 'db';
+$db = 'mydatabase';
+$user = 'user';
+$password = 'password';
+$charset = 'utf8mb4';
+//opties
+$opties = [
+PDO ::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+PDO ::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+PDO ::ATTR_EMULATE_PREPARES => false,
+];
+//dsn = data source name
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+try {
+  //create the contection
+  $pdo = new PDO($dsn, $user, $password, $opties);
+  //succes melding
+  echo "Database connection goed <br/>";
+} catch (PDOException $e) {
+  //fout melding
+  echo $e->getMessage();
+  //stop (die)
+  die("sorry, database probleem"); 
+}
+ $sql = "SELECT * FROM studenten WHERE leeftijd > 16";
+ 
+ $statement = $pdo->prepare($sql);
+
+ $statement->execute();
+
+ $studenten = $statement->fetchAll();  
+
+ echo "<pre>";
+ print_r($studenten);
+  echo "</pre>";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
